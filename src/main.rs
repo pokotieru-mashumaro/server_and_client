aff8f97bc74d80b91f1241bd92dedf2acd7363a8
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-07-25 16:40:16 +0900

    mainで条件分岐

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 mod ch5;
+use ch5::client::client_main;
+use ch5::server::server_main;
 
-use ch5::client;
-use ch5::server;
+use std::env;
 
 fn main() {
-    println!("hello");
+    let av: Vec<String> = env::args().collect();
+
+    match av.len() {
+        2 => {
+            match &*av[1] {
+                "server" => server_main(),
+                "client" => client_main(),
+                _ => {
+                    println!("Bad Args😡😡😡");
+                    println!("Args: server client");
+                }
+            }
+        }
+        _ => {
+            println!("Give two Args😡😡😡");
+            println!("Args: server client");
+        }
+    }
+
 }
